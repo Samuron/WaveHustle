53bb0d9e5b0b096d435cfe4b4304f6c9cbe45b47
oefimenko <oefimenko@oefimenkos-MBP.mshome.net>
2016-12-02 16:08:14 +0200

    Add thread

--- src/AddThread.js
+++ src/AddThread.js
@@ -4,12 +4,14 @@ import { List, ListItem } from 'material-ui/List';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
 
 import { values, reverse } from 'lodash';
 
 const style = {
-  width: '100%'
+  width: '100%',
+  heigth: '100%'
 };
 
 export default class AddThread extends Component {
@@ -32,16 +34,14 @@ export default class AddThread extends Component {
     })
   }
 
-  handleOpen() { 
-    this.setState({ open: true }); 
-  };
-
-  handleClose() { 
+  handleOpen = () => { this.setState({ open: true }); };
+  handleClose = () => { 
+    this.clear();
     this.setState({ open: false }); 
   };
 
-  postThread() {
-    if (this.refs.name.getValue() == "") {
+  postThread = () => {
+    if (this.state.name == "") {
       this.clear();
       return;
     }
@@ -49,8 +49,8 @@ export default class AddThread extends Component {
     var thread = firebase.database().ref('/threads/').push({
       creator: this.state.userName,
       creatorPhotoUrl: this.state.userPhotoUrl,
-      name: this.refs.name.getValue(),
-      photoUrl: this.refs.photoUrl.getValue(),
+      name: this.state.name,
+      photoUrl: this.state.photoUrl,
       isPrivate: false
     });
     this.clear();
@@ -73,17 +73,21 @@ export default class AddThread extends Component {
 
     return (
       <div>
+        <RaisedButton label="Add event" onTouchTap={this.handleOpen} />
         <Dialog title="Create new thread" actions={actions} modal={true} open={this.state.open}>
+          <br />
+          Whats is it about?
+          <br />
           <TextField
             hintText="Name"
-            ref="name"
             value={this.state.name}
             onChange={(event) => this.setState({ name: event.target.value })}
-            />
+          />
+          <br />
+          Personalize it!
           <br />
           <TextField
             hintText="Add picture link"
-            ref="photoUrl"
             value={this.state.photoUrl}
             onChange={(event) => this.setState({ photoUrl: event.target.value })}
           />
