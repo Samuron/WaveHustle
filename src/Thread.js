fd93b2c37a1f2308b50853e62e7a60bc535ee98b
Ievgen Makukh <samuronchik@gmail.com>
2016-12-02 15:35:34 +0200

    Adding and showing events

--- src/Thread.js
+++ src/Thread.js
@@ -10,16 +10,29 @@ export default class Thread extends Component {
   constructor(props) {
     super(props);
     this.addNewMessage = this.addNewMessage.bind(this);
-
+    const { threadId } = this.props.params;
+    this.threadRf = firebase.database().ref(`/threads/${threadId}`);
+    this.eventsRf = firebase.database().ref(`/threads/${threadId}/events`);
     this.state = {
       messages: [],
+      events: [],
       user: firebase.auth().currentUser,
     }
   }
 
   componentWillMount() {
-    const { threadId } = this.props.params;
-    this.threadRf = firebase.database().ref(`/threads/${threadId}`);
+    this.eventsRf.on('value', (snapshot) => {
+      var events = [];
+      snapshot.forEach((child) => {
+        var item = child.val();
+        item['.key'] = child.key;
+        events.push(item);
+      });
+
+      this.setState({
+        events: events
+      });
+    });
 
     this.threadRf
       .on('value', snapshot => {
@@ -35,6 +48,7 @@ export default class Thread extends Component {
           messages: orderBy(messages, 'time')
         });
       });
+
   }
 
   addNewMessage(messageText) {
@@ -54,7 +68,7 @@ export default class Thread extends Component {
     return (
       <div>
         <AddEvent threadId={this.props.params.threadId} />
-        <EventCard style={{ width: 300, background: 'red'}} events={[]} />
+        <EventCard style={{ width: 300, background: 'red'}} events={this.state.events} />
         <Chat onMessageSubmit={this.addNewMessage} messages={this.state.messages} />
       </div>
     );
