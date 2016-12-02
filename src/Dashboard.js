38a040e86b5f15088ff5dc181352935b3be5a720
ekaretin <rincewind3@gmail.com>
2016-12-02 13:39:58 +0200

    add some buttons to dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import {List, ListItem} from 'material-ui/List';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentClear from 'material-ui/svg-icons/content/clear';
 
-const style = {
-  background: '#000'
+const listStyle = {
+  background: '#000',
+  marginBottom: '1em'
+};
+
+const listItemTitleStyle = {
+  verticalAlign: 'middle'
+};
+
+const listItemIconStyle = {
+  position: 'absolute',
+  right: '2em',
+  top: '1em'
 };
 
 export default class Dashboard extends Component {
@@ -16,12 +30,35 @@ export default class Dashboard extends Component {
           id: 'povidlo'
         }
       ]
+    };
+    this.removing = false;
+  }
+
+  openThread() {
+    if(!this.removing){
+      console.log('open thread');
     }
+  }
+
+  addThread() {
+
+  }
+
+  removeThread() {
+    console.log('remove thread');
+    this.removing = true;
+    setTimeout(() => {
+      this.removing = false;
+    }, 1000);
   }
 
   render() {
     return (
-      <DashboardComponent threads={this.state.threads} />
+      <DashboardComponent threads={this.state.threads}
+                          openThread={this.openThread.bind(this)}
+                          addThread={this.addThread.bind(this)}
+                          removeThread={this.removeThread.bind(this)}
+      />
     );
   }
 }
@@ -35,15 +72,22 @@ class DashboardComponent extends Component {
 
     return (
       <div>
-        <List  style={style}>
+        <List style={listStyle}>
           {
             this.props.threads.map((thread) => {
               return (
-                <ListItem key={thread.id}>{thread.name}: {thread.id}</ListItem>
+                <ListItem onClick={this.props.openThread} key={thread.id}>
+                  <div style={listItemTitleStyle}>{thread.name}: {thread.id}</div>
+                  <ContentClear onClick={this.props.removeThread} style={listItemIconStyle}/>
+                </ListItem>
               );
             })
           }
         </List>
+        <FloatingActionButton onClick={this.props.addThread}>
+          <ContentAdd />
+        </FloatingActionButton>
+
 
       </div>
     );
