b7a663f2c5e35bec616fa31fa5b469574941a16c
ekaretin <rincewind3@gmail.com>
2016-12-02 17:49:10 +0200

    add user with ids

--- src/Thread.js
+++ src/Thread.js
@@ -6,6 +6,14 @@ import firebase from 'firebase';
 import { map, orderBy } from 'lodash';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+const styles = {
+  customWidth: {
+    background: '#333'
+  },
+};
 
 export default class Thread extends Component {
   constructor(props) {
@@ -14,6 +22,7 @@ export default class Thread extends Component {
     const { threadId } = this.props.params;
     this.threadRf = firebase.database().ref(`/threads/${threadId}`);
     this.eventsRf = firebase.database().ref(`/threads/${threadId}/events`);
+    this.usersRf =  firebase.database().ref(`/users`);
 
     this.state = {
       messages: [],
@@ -22,7 +31,9 @@ export default class Thread extends Component {
       thread: {
         isPrivate: true
       },
-      searchQuery: ''
+      searchQuery: '',
+      choosedUser: '',
+      users: []
     }
   }
 
@@ -56,6 +67,20 @@ export default class Thread extends Component {
         });
       });
 
+    this.usersRf
+      .on('value', snapshot => {
+        const users = snapshot.val();
+        const output = [];
+        Object.keys(users).forEach(userKey => {
+          output.push({...users[userKey], id: userKey });
+        });
+        this.setState({users: output});
+      });
+
+  }
+
+  filterUsers() {
+    return this.state.users.filter(user => !this.state.thread.users.includes(user.id));
   }
 
   addNewMessage(messageText) {
@@ -77,6 +102,12 @@ export default class Thread extends Component {
     });
   }
 
+  addUser(e, index, value) {
+    this.threadRf.update({
+      users: [...this.state.thread.users, value]
+    });
+  }
+
   render() {
     const events = !this.state.searchQuery ? this.state.events : this.state.events.filter(event => {
       const name = event.name.toLowerCase();
@@ -92,9 +123,16 @@ export default class Thread extends Component {
       <div style={{ width: 1200, margin: '0 auto'}}>
         <div className="event-content" style={{ width: 870, float: 'left'}}>
           <AddEvent threadId={this.props.params.threadId} />
-          {this.state.thread.isPrivate ? '' : (<RaisedButton label="Make private"
+          {this.state.thread.isPrivate ? (
+            <DropDownMenu style={styles.customWidth} value={this.state.choosedUser} onChange={this.addUser.bind(this)}>
+              <MenuItem key={-1} value={''} primaryText={'add some user'} />
+              {this.filterUsers().map((user, index) => (<MenuItem key={index} value={user.id} primaryText={user.displayName} />))}
+            </DropDownMenu>
+          ) :
+            (<RaisedButton label="Make private"
                         onTouchTap={this.makePrivate.bind(this)}>
-          </RaisedButton>)}
+          </RaisedButton>)
+          }
           <div style={{background: 'rgb(48, 48, 48)', margin: '10px 0 0 0', padding: '0 10px'}}>
             <TextField hintText="Search...."
                        fullWidth={true}
